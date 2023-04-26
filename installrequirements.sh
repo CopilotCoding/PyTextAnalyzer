810535a58bdf770a6448754366bc1740781dca2d
CopilotCoding <105129338+CopilotCoding@users.noreply.github.com>
2023-04-26 15:49:46 -0700

    Update installrequirements.sh

--- installrequirements.sh
+++ installrequirements.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pip install setuptools
+
 # Install requests
 pip install requests
 
@@ -8,4 +9,17 @@ pip install requests
 pip install beautifulsoup4
 
 pip install numpy
+
 pip install nltk
+
+# Install textblob
+pip install textblob
+
+# Install spacy
+pip install spacy
+
+# Install gensim
+pip install gensim
+
+# Install yake
+pip install yake
