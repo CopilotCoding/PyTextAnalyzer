5adb7b3623c97f5d7c74ce0d0c6300ef9e721ac2
CopilotCoding <105129338+CopilotCoding@users.noreply.github.com>
2023-04-26 16:07:49 -0700

    Update installrequirements.sh

--- installrequirements.sh
+++ installrequirements.sh
@@ -3,6 +3,9 @@
 # Install packages from requirements.txt
 pip3 install -r requirements.txt
 
+# Install textblob corpora
+python3 -m textblob.download_corpora
+
 # Wait for user input
 read -p "Press Enter to exit"
 
